38b2e89beb932454c7af1d276ced7a970390460e
TomasVotruba <tomas.vot@gmail.com>
2020-07-08 15:40:37 +0200

    [FeatureShifter] Merge symplify/autodiscoery feature to ConfigTransformer

---
+++ packages/feature-shifter/src/ValueObject/YamlKey.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FeatureShifter\ValueObject;
+
+final class YamlKey
+{
+    /**
+     * @var string
+     */
+    public const DEFAULTS = '_defaults';
+
+    /**
+     * @var string
+     */
+    public const SERVICES = 'services';
+
+    /**
+     * @var string
+     */
+    public const AUTOWIRE = 'autowire';
+
+    /**
+     * @var string
+     */
+    public const TAGS = 'tags';
+
+    /**
+     * @var string
+     */
+    public const AUTOCONFIGURE = 'autoconfigure';
+
+    /**
+     * @var string
+     */
+    public const RESOURCE = 'resource';
+}
---
+++ packages/format-switcher/src/ValueObject/Option.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\ValueObject;
+
+final class Option
+{
+    /**
+     * @var string
+     */
+    public const SOURCE = 'source';
+
+    /**
+     * @var string
+     */
+    public const INPUT_FORMAT = 'input-format';
+
+    /**
+     * @var string
+     */
+    public const OUTPUT_FORMAT = 'output-format';
+
+    /**
+     * @var string
+     */
+    public const DELETE = 'delete';
+
+    /**
+     * @var string
+     */
+    public const TARGET_SYMFONY_VERSION = 'target-symfony-version';
+}
