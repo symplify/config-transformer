b9d963a855234c695abb8df93a09d98112cca87e
Tomas Votruba <tomas.vot@gmail.com>
2020-07-10 20:20:00 +0200

    Merge pull request #46 from migrify/fix-exclude-single-value
    [ConfigTransformer] exclude is only one array value

---
+++ packages/format-switcher/src/Converter/ConfigFormatConverter.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\Converter;
+
+use Migrify\ConfigTransformer\FormatSwitcher\ConfigLoader;
+use Migrify\ConfigTransformer\FormatSwitcher\DependencyInjection\ContainerBuilderCleaner;
+use Migrify\ConfigTransformer\FormatSwitcher\DumperFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\DumperFomatter\YamlDumpFormatter;
+use Migrify\ConfigTransformer\FormatSwitcher\Exception\NotImplementedYetException;
+use Migrify\ConfigTransformer\FormatSwitcher\Exception\ShouldNotHappenException;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\Format;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @see \Migrify\ConfigTransformer\FormatSwitcher\Tests\Converter\ConfigFormatConverter\ConfigFormatConverterTest
+ */
+final class ConfigFormatConverter
+{
+    /**
+     * @var ConfigLoader
+     */
+    private $configLoader;
+
+    /**
+     * @var DumperFactory
+     */
+    private $dumperFactory;
+
+    /**
+     * @var ContainerBuilderCleaner
+     */
+    private $containerBuilderCleaner;
+
+    /**
+     * @var YamlDumpFormatter
+     */
+    private $yamlDumpFormatter;
+
+    /**
+     * @var YamlToPhpConverter
+     */
+    private $yamlToPhpConverter;
+
+    public function __construct(
+        ConfigLoader $configLoader,
+        DumperFactory $dumperFactory,
+        ContainerBuilderCleaner $containerBuilderCleaner,
+        YamlDumpFormatter $yamlDumpFormatter,
+        YamlToPhpConverter $yamlToPhpConverter
+    ) {
+        $this->configLoader = $configLoader;
+        $this->dumperFactory = $dumperFactory;
+        $this->containerBuilderCleaner = $containerBuilderCleaner;
+        $this->yamlDumpFormatter = $yamlDumpFormatter;
+        $this->yamlToPhpConverter = $yamlToPhpConverter;
+    }
+
+    public function convert(SmartFileInfo $smartFileInfo, string $inputFormat, string $outputFormat): string
+    {
+        $containerBuilder = $this->configLoader->createAndLoadContainerBuilderFromFileInfo($smartFileInfo);
+
+        if ($outputFormat === Format::YAML) {
+            return $this->dumpContainerBuilderToYaml($containerBuilder);
+        }
+
+        if ($outputFormat === Format::PHP) {
+            if ($inputFormat === Format::YAML) {
+                return $this->yamlToPhpConverter->convert($smartFileInfo->getContents());
+            }
+
+            if ($inputFormat === Format::XML) {
+                $yamlContent = $this->dumpContainerBuilderToYaml($containerBuilder);
+                return $this->yamlToPhpConverter->convert($yamlContent);
+            }
+        }
+
+        throw new NotImplementedYetException($outputFormat);
+    }
+
+    private function dumpContainerBuilderToYaml(ContainerBuilder $containerBuilder): string
+    {
+        $dumper = $this->dumperFactory->createFromContainerBuilderAndOutputFormat($containerBuilder, Format::YAML);
+        $this->containerBuilderCleaner->cleanContainerBuilder($containerBuilder);
+
+        $content = $dumper->dump();
+        if (! is_string($content)) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->yamlDumpFormatter->format($content);
+    }
+}
