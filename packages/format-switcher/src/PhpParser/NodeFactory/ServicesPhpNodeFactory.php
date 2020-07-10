69f6212d23d5a46fb22c2ed79ef8039cbfaa940d
TomasVotruba <tomas.vot@gmail.com>
2020-07-10 21:32:09 +0200

    various node-related improvements

--- packages/format-switcher/src/PhpParser/NodeFactory/ServicesPhpNodeFactory.php
+++ packages/format-switcher/src/PhpParser/NodeFactory/ServicesPhpNodeFactory.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory;
 
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\VariableName;
+use PhpParser\BuilderHelpers;
 use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Expression;
 
 final class ServicesPhpNodeFactory
 {
     /**
      * @var string
      */
-    private const SERVICES_VARIABLE_NAME = 'services';
+    private const EXCLUDE = 'exclude';
 
     /**
      * @var PhpNodeFactory
@@ -26,9 +30,54 @@ final class ServicesPhpNodeFactory
         $this->phpNodeFactory = $phpNodeFactory;
     }
 
-    public function createServicesLoadMethodCall(string $serviceKey, $serviceValues): MethodCall
+    public function createServicesInit(): Expression
     {
-        $servicesVariable = new Variable(self::SERVICES_VARIABLE_NAME);
+        $servicesVariable = $this->createServicesVariable();
+        $containerConfiguratorVariable = new Variable(VariableName::CONTAINER_CONFIGURATOR);
+
+        $assign = new Assign($servicesVariable, new MethodCall($containerConfiguratorVariable, 'services'));
+
+        return new Expression($assign);
+    }
+
+    public function createResource(string $serviceKey, array $serviceValues): Expression
+    {
+        $servicesLoadMethodCall = $this->createServicesLoadMethodCall($serviceKey, $serviceValues);
+
+        if (! isset($serviceValues[self::EXCLUDE])) {
+            return new Expression($servicesLoadMethodCall);
+        }
+
+        $exclude = $serviceValues[self::EXCLUDE];
+        $excludeMethodCall = new MethodCall($servicesLoadMethodCall, self::EXCLUDE);
+
+        $excludeValue = [];
+        foreach ($exclude as $key => $singleExclude) {
+            $excludeValue[$key] = $this->phpNodeFactory->createAbsoluteDirExpr($singleExclude);
+        }
+
+        $excludeValue = BuilderHelpers::normalizeValue($excludeValue);
+        $excludeMethodCall->args[] = new Arg($excludeValue);
+
+        return new Expression($excludeMethodCall);
+    }
+
+    public function createServiceDefaults(array $serviceValues): Expression
+    {
+        $methodCall = new MethodCall($this->createServicesVariable(), 'defaults');
+
+        foreach (array_keys($serviceValues) as $key) {
+            if (in_array($key, ['autowire', 'autoconfigure', 'public'], true)) {
+                $methodCall = new MethodCall($methodCall, $key);
+            }
+        }
+
+        return new Expression($methodCall);
+    }
+
+    private function createServicesLoadMethodCall(string $serviceKey, $serviceValues): MethodCall
+    {
+        $servicesVariable = $this->createServicesVariable();
 
         $resource = $serviceValues['resource'];
 
@@ -37,5 +86,10 @@ final class ServicesPhpNodeFactory
         $args[] = new Arg($this->phpNodeFactory->createAbsoluteDirExpr($resource));
 
         return new MethodCall($servicesVariable, 'load', $args);
+    }
+
+    private function createServicesVariable(): Variable
+    {
+        return new Variable(VariableName::SERVICES);
     }
 }
--- packages/format-switcher/src/PhpParser/Printer/FluentMethodCallPrinter.php
+++ packages/format-switcher/src/PhpParser/Printer/FluentMethodCallPrinter.php
@@ -5,17 +5,43 @@ declare(strict_types=1);
 namespace Migrify\ConfigTransformer\FormatSwitcher\PhpParser\Printer;
 
 use Nette\Utils\Strings;
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\PrettyPrinter\Standard;
 
 final class FluentMethodCallPrinter extends Standard
 {
+    /**
+     * @var string
+     */
+    private const EOL_CHAR = "\n";
+
+    public function prettyPrintFile(array $stmts): string
+    {
+        $printedContent = parent::prettyPrintFile($stmts);
+
+        // remove trailing spaces
+        $printedContent = Strings::replace($printedContent, '#^[ ]+\n#m', "\n");
+
+        return $printedContent . self::EOL_CHAR;
+    }
+
+    protected function pExpr_Array(Array_ $array): string
+    {
+        $array->setAttribute('kind', Array_::KIND_SHORT);
+
+        return parent::pExpr_Array($array);
+    }
+
     protected function pExpr_MethodCall(MethodCall $methodCall): string
     {
         $printedMethodCall = parent::pExpr_MethodCall($methodCall);
+        return $this->indentFluentCallToNewline($printedMethodCall);
+    }
 
+    private function indentFluentCallToNewline(string $content): string
+    {
         $nextCallIndentReplacement = ')' . PHP_EOL . Strings::indent('->', 8, ' ');
-
-        return Strings::replace($printedMethodCall, '#\)->#', $nextCallIndentReplacement);
+        return Strings::replace($content, '#\)->#', $nextCallIndentReplacement);
     }
 }
