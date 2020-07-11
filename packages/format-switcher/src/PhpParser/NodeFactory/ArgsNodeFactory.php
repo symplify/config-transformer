8dcdd73751250bcc191e7e97bf66c4683a02896f
TomasVotruba <tomas.vot@gmail.com>
2020-07-11 10:58:49 +0200

    prepare

---
+++ packages/format-switcher/src/PhpParser/NodeFactory/ArgsNodeFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory;
+
+final class ArgsNodeFactory
+{
+    public function createFromValues($values)
+    {
+        dump($values);
+        die;
+    }
+}
