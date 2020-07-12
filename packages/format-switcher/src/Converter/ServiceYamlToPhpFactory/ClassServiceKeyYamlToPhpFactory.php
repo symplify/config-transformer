da823db04559a81a4a6e142bdce7f522b77d637a
TomasVotruba <tomas.vot@gmail.com>
2020-07-12 12:32:05 +0200

    decouple ClassServiceKeyYamlToPhpFactory

---
+++ packages/format-switcher/src/Converter/ServiceYamlToPhpFactory/ClassServiceKeyYamlToPhpFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\Converter\ServiceYamlToPhpFactory;
+
+use Migrify\ConfigTransformer\FormatSwitcher\Contract\Converter\ServiceKeyYamlToPhpFactoryInterface;
+use Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory\ArgsNodeFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory\Service\ServiceOptionNodeFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\VariableName;
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Expression;
+
+/**
+ * Handles this part:
+ *
+ * services:
+ *     Some:
+ *         class: Other <---
+ */
+final class ClassServiceKeyYamlToPhpFactory implements ServiceKeyYamlToPhpFactoryInterface
+{
+    /**
+     * @var string
+     */
+    private const CLASS_KEY = 'class';
+
+    /**
+     * @var ArgsNodeFactory
+     */
+    private $argsNodeFactory;
+
+    /**
+     * @var ServiceOptionNodeFactory
+     */
+    private $serviceOptionNodeFactory;
+
+    public function __construct(
+        ArgsNodeFactory $argsNodeFactory,
+        ServiceOptionNodeFactory $serviceOptionNodeFactory
+    ) {
+        $this->argsNodeFactory = $argsNodeFactory;
+        $this->serviceOptionNodeFactory = $serviceOptionNodeFactory;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function convertYamlToNodes($key, $yaml): array
+    {
+        $nodes = [];
+
+        $args = $this->argsNodeFactory->createFromValues([$key, $yaml[self::CLASS_KEY]]);
+        $setMethodCall = new MethodCall(new Variable(VariableName::SERVICES), 'set', $args);
+
+        unset($yaml[self::CLASS_KEY]);
+
+        $setMethodCall = $this->serviceOptionNodeFactory->convertServiceOptionsToNodes($yaml, $setMethodCall);
+        $nodes[] = new Expression($setMethodCall);
+
+        return $nodes;
+    }
+
+    public function isMatch($key, $values): bool
+    {
+        return isset($values[self::CLASS_KEY]);
+    }
+}
