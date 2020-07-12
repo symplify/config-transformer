928514693c1fef796325b38e5820feb278628af4
TomasVotruba <tomas.vot@gmail.com>
2020-07-12 10:51:57 +0200

    decouple DefaultsServiceKeyYamlToPhpFactory

--- packages/format-switcher/src/PhpParser/NodeFactory/ServicesPhpNodeFactory.php
+++ packages/format-switcher/src/PhpParser/NodeFactory/ServicesPhpNodeFactory.php
@@ -7,20 +7,13 @@ namespace Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory;
 use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\VariableName;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Name;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Expression;
 
 final class ServicesPhpNodeFactory
 {
-    /**
-     * @var string
-     */
-    private const BIND = 'bind';
-
     /**
      * @var string
      */
@@ -44,7 +37,7 @@ final class ServicesPhpNodeFactory
 
     public function createServicesInit(): Expression
     {
-        $servicesVariable = $this->createServicesVariable();
+        $servicesVariable = new Variable(VariableName::SERVICES);
         $containerConfiguratorVariable = new Variable(VariableName::CONTAINER_CONFIGURATOR);
 
         $assign = new Assign($servicesVariable, new MethodCall($containerConfiguratorVariable, 'services'));
@@ -75,29 +68,9 @@ final class ServicesPhpNodeFactory
         return new Expression($excludeMethodCall);
     }
 
-    public function createServiceDefaults(array $serviceValues): Expression
-    {
-        $methodCall = new MethodCall($this->createServicesVariable(), 'defaults');
-
-        foreach ($serviceValues as $key => $value) {
-            if (in_array($key, ['autowire', 'autoconfigure', 'public'], true)) {
-                $methodCall = new MethodCall($methodCall, $key);
-                if ($value === false) {
-                    $methodCall->args[] = new Arg($this->createFalse());
-                }
-            }
-
-            if ($key === self::BIND) {
-                $methodCall = $this->createBindMethodCall($methodCall, $serviceValues[self::BIND]);
-            }
-        }
-
-        return new Expression($methodCall);
-    }
-
     private function createServicesLoadMethodCall(string $serviceKey, $serviceValues): MethodCall
     {
-        $servicesVariable = $this->createServicesVariable();
+        $servicesVariable = new Variable(VariableName::SERVICES);
 
         $resource = $serviceValues['resource'];
 
@@ -106,25 +79,5 @@ final class ServicesPhpNodeFactory
         $args[] = new Arg($this->commonNodeFactory->createAbsoluteDirExpr($resource));
 
         return new MethodCall($servicesVariable, 'load', $args);
-    }
-
-    private function createBindMethodCall(MethodCall $methodCall, array $bindValues): MethodCall
-    {
-        foreach ($bindValues as $key => $value) {
-            $args = $this->argsNodeFactory->createFromValues([$key, $value]);
-            $methodCall = new MethodCall($methodCall, self::BIND, $args);
-        }
-
-        return $methodCall;
-    }
-
-    private function createServicesVariable(): Variable
-    {
-        return new Variable(VariableName::SERVICES);
-    }
-
-    private function createFalse(): ConstFetch
-    {
-        return new ConstFetch(new Name('false'));
     }
 }
