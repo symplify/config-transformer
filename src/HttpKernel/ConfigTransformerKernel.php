2ae3c5cddc400a47e7e3c7adbf52ce1b368fb0a2
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 01:07:53 +0200

    [ConfigTransformer] Switch YAML to PHP configs

--- src/HttpKernel/ConfigTransformerKernel.php
+++ src/HttpKernel/ConfigTransformerKernel.php
@@ -20,7 +20,7 @@ final class ConfigTransformerKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__ . '/../../config/config.yaml');
+        $loader->load(__DIR__ . '/../../config/config.php');
     }
 
     public function getCacheDir(): string
