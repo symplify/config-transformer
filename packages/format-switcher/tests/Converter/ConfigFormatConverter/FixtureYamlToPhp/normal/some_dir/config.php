59eebc2bef4532ac6dfed4c947f31e29d16850ab
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 17:06:20 +0200

    [ConfigTransformer] Add import or PHP/glob support

--- packages/format-switcher/tests/Converter/ConfigFormatConverter/FixtureYamlToPhp/normal/some_dir/config.php
+++ packages/format-switcher/tests/Converter/ConfigFormatConverter/FixtureYamlToPhp/normal/some_dir/config.php
@@ -1,3 +1,9 @@
 <?php
 
-declare(strict_types=1);
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('key', 'value');
+};
