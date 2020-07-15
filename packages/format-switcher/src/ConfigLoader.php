9f443a42a3a296a4e7ef27abefb54331cacb33d9
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 18:55:01 +0200

    add support for older int constants

--- packages/format-switcher/src/ConfigLoader.php
+++ packages/format-switcher/src/ConfigLoader.php
@@ -7,6 +7,7 @@ namespace Migrify\ConfigTransformer\FormatSwitcher;
 use Migrify\ConfigTransformer\FormatSwitcher\DependencyInjection\LoaderFactory\IdAwareXmlFileLoaderFactory;
 use Migrify\ConfigTransformer\FormatSwitcher\Exception\NotImplementedYetException;
 use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\Format;
+use Nette\Utils\Strings;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\Loader;
@@ -16,6 +17,7 @@ use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class ConfigLoader
 {
@@ -24,9 +26,15 @@ final class ConfigLoader
      */
     private $idAwareXmlFileLoaderFactory;
 
-    public function __construct(IdAwareXmlFileLoaderFactory $idAwareXmlFileLoaderFactory)
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    public function __construct(IdAwareXmlFileLoaderFactory $idAwareXmlFileLoaderFactory, SmartFileSystem $smartFileSystem)
     {
         $this->idAwareXmlFileLoaderFactory = $idAwareXmlFileLoaderFactory;
+        $this->smartFileSystem = $smartFileSystem;
     }
 
     public function createAndLoadContainerBuilderFromFileInfo(SmartFileInfo $smartFileInfo): ContainerBuilder
@@ -34,6 +42,16 @@ final class ConfigLoader
         $containerBuilder = new ContainerBuilder();
 
         $loader = $this->createLoaderBySuffix($containerBuilder, $smartFileInfo->getSuffix());
+
+        // correct old syntax of tags so we can parse it
+        $content = $smartFileInfo->getContents();
+        if (in_array($smartFileInfo->getSuffix(), ['yml', 'yaml'], true)) {
+            $content = Strings::replace($content, '#\!php\/const\:( )?#', '!php/const ');
+            if ($content !== $smartFileInfo->getContents()) {
+                $this->smartFileSystem->dumpFile($smartFileInfo->getRealPath(), $content);
+            }
+        }
+
         $loader->load($smartFileInfo->getRealPath());
 
         return $containerBuilder;
