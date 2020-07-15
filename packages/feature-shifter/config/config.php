5a1b52c325c4d1b0d5c373b5cc59abdc374a450d
TomasVotruba <tomas.vot@gmail.com>
2020-07-16 01:34:58 +0200

    add missing deps

--- packages/feature-shifter/config/config.php
+++ packages/feature-shifter/config/config.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Symplify\SmartFileSystem\SmartFileSystem;
+
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
@@ -14,4 +16,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Migrify\ConfigTransformer\FeatureShifter\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/Utils/*']);
+
+    $services->set(SmartFileSystem::class);
 };
