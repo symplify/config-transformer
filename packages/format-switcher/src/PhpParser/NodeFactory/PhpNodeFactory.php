54a6455997d1c212b738b27857b508c89897a801
TomasVotruba <tomas.vot@gmail.com>
2020-07-16 22:55:43 +0200

    [ConfigTransformer] Prefix with __DIR__, if possible parameter path

--- packages/format-switcher/src/PhpParser/NodeFactory/PhpNodeFactory.php
+++ packages/format-switcher/src/PhpParser/NodeFactory/PhpNodeFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory;
 
+use Migrify\ConfigTransformer\FormatSwitcher\Provider\CurrentFilePathProvider;
 use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\VariableName;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Expression;
@@ -16,17 +18,60 @@ final class PhpNodeFactory
      */
     private $argsNodeFactory;
 
-    public function __construct(ArgsNodeFactory $argsNodeFactory)
-    {
+    /**
+     * @var CurrentFilePathProvider
+     */
+    private $currentFilePathProvider;
+
+    /**
+     * @var CommonNodeFactory
+     */
+    private $commonNodeFactory;
+
+    public function __construct(
+        ArgsNodeFactory $argsNodeFactory,
+        CommonNodeFactory $commonNodeFactory,
+        CurrentFilePathProvider $currentFilePathProvider
+    ) {
         $this->argsNodeFactory = $argsNodeFactory;
+        $this->currentFilePathProvider = $currentFilePathProvider;
+        $this->commonNodeFactory = $commonNodeFactory;
     }
 
     public function createParameterSetMethodCall(string $parameterName, $value): Expression
     {
+        if (is_string($value)) {
+            $value = $this->prefixWithDirConstantIfExistingPath($value);
+        }
+
+        if (is_array($value)) {
+            foreach ($value as $subKey => $subValue) {
+                if (! is_string($subValue)) {
+                    continue;
+                }
+                $value[$subKey] = $this->prefixWithDirConstantIfExistingPath($subValue);
+            }
+        }
+
         $args = $this->argsNodeFactory->createFromValues([$parameterName, $value]);
 
         $parametersVariable = new Variable(VariableName::PARAMETERS);
         $methodCall = new MethodCall($parametersVariable, 'set', $args);
         return new Expression($methodCall);
+    }
+
+    /**
+     * @return Expr|string
+     */
+    private function prefixWithDirConstantIfExistingPath(string $value)
+    {
+        $configDirectory = dirname($this->currentFilePathProvider->getFilePath());
+
+        $possibleConfigPath = $configDirectory . '/' . $value;
+        if (is_file($possibleConfigPath) || is_dir($possibleConfigPath)) {
+            return $this->commonNodeFactory->createAbsoluteDirExpr($value);
+        }
+
+        return $value;
     }
 }
