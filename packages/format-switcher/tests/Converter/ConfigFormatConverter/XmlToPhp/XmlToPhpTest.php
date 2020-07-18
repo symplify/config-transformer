e00e5ed502e5dc184c37373a1936fe669b3e62fb
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 03:02:50 +0200

    move tests to particular namespace

--- packages/format-switcher/tests/Converter/ConfigFormatConverter/XmlToPhp/XmlToPhpTest.php
+++ packages/format-switcher/tests/Converter/ConfigFormatConverter/XmlToPhp/XmlToPhpTest.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Migrify\ConfigTransformer\FormatSwitcher\Tests\Converter\ConfigFormatConverter;
+namespace Migrify\ConfigTransformer\FormatSwitcher\Tests\Converter\ConfigFormatConverter\XmlToPhp;
 
 use Iterator;
+use Migrify\ConfigTransformer\FormatSwitcher\Tests\Converter\ConfigFormatConverter\AbstractConfigFormatConverterTest;
 use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -20,6 +21,6 @@ final class XmlToPhpTest extends AbstractConfigFormatConverterTest
 
     public function provideData(): Iterator
     {
-        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/FixtureXmlToPhp', '*.xml');
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.xml');
     }
 }
