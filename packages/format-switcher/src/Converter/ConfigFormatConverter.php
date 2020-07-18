3aaa028a632be204d00d1f899cef44b2316a9e86
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 09:58:23 +0200

    [ConfigTransformer] Skip basic non-class params

---
+++ packages/format-switcher/src/Converter/ConfigFormatConverter.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\Converter;
+
+use Migrify\ConfigTransformer\FormatSwitcher\ConfigLoader;
+use Migrify\ConfigTransformer\FormatSwitcher\DependencyInjection\ContainerBuilderCleaner;
+use Migrify\ConfigTransformer\FormatSwitcher\DumperFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\DumperFomatter\YamlDumpFormatter;
+use Migrify\ConfigTransformer\FormatSwitcher\Exception\NotImplementedYetException;
+use Migrify\ConfigTransformer\FormatSwitcher\Exception\ShouldNotHappenException;
+use Migrify\ConfigTransformer\FormatSwitcher\Provider\CurrentFilePathProvider;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\Format;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ConfigFormatConverter
+{
+    /**
+     * @var ConfigLoader
+     */
+    private $configLoader;
+
+    /**
+     * @var DumperFactory
+     */
+    private $dumperFactory;
+
+    /**
+     * @var ContainerBuilderCleaner
+     */
+    private $containerBuilderCleaner;
+
+    /**
+     * @var YamlDumpFormatter
+     */
+    private $yamlDumpFormatter;
+
+    /**
+     * @var YamlToPhpConverter
+     */
+    private $yamlToPhpConverter;
+
+    /**
+     * @var CurrentFilePathProvider
+     */
+    private $currentFilePathProvider;
+
+    public function __construct(
+        ConfigLoader $configLoader,
+        DumperFactory $dumperFactory,
+        ContainerBuilderCleaner $containerBuilderCleaner,
+        YamlDumpFormatter $yamlDumpFormatter,
+        YamlToPhpConverter $yamlToPhpConverter,
+        CurrentFilePathProvider $currentFilePathProvider
+    ) {
+        $this->configLoader = $configLoader;
+        $this->dumperFactory = $dumperFactory;
+        $this->containerBuilderCleaner = $containerBuilderCleaner;
+        $this->yamlDumpFormatter = $yamlDumpFormatter;
+        $this->yamlToPhpConverter = $yamlToPhpConverter;
+        $this->currentFilePathProvider = $currentFilePathProvider;
+    }
+
+    public function convert(SmartFileInfo $smartFileInfo, string $inputFormat, string $outputFormat): string
+    {
+        $this->currentFilePathProvider->setFilePath($smartFileInfo->getRealPath());
+
+        $containerBuilderAndFileContent = $this->configLoader->createAndLoadContainerBuilderFromFileInfo(
+            $smartFileInfo
+        );
+
+        $containerBuilder = $containerBuilderAndFileContent->getContainerBuilder();
+
+        if ($outputFormat === Format::YAML) {
+            return $this->dumpContainerBuilderToYaml($containerBuilder);
+        }
+
+        if ($outputFormat === Format::PHP) {
+            if ($inputFormat === Format::YAML) {
+                return $this->yamlToPhpConverter->convert($containerBuilderAndFileContent->getFileContent());
+            }
+
+            if ($inputFormat === Format::XML) {
+                $yamlContent = $this->dumpContainerBuilderToYaml($containerBuilder);
+                return $this->yamlToPhpConverter->convert($yamlContent);
+            }
+        }
+
+        throw new NotImplementedYetException($outputFormat);
+    }
+
+    private function dumpContainerBuilderToYaml(ContainerBuilder $containerBuilder): string
+    {
+        $dumper = $this->dumperFactory->createFromContainerBuilderAndOutputFormat($containerBuilder, Format::YAML);
+        $this->containerBuilderCleaner->cleanContainerBuilder($containerBuilder);
+
+        $content = $dumper->dump();
+        if (! is_string($content)) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->yamlDumpFormatter->format($content);
+    }
+}
---
+++ packages/format-switcher/src/Converter/ServiceKeyYamlToPhpFactory/ConfiguredServiceKeyYamlToPhpFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\Converter\ServiceKeyYamlToPhpFactory;
+
+use Migrify\ConfigTransformer\FeatureShifter\ValueObject\YamlKey;
+use Migrify\ConfigTransformer\FormatSwitcher\Contract\Converter\ServiceKeyYamlToPhpFactoryInterface;
+use Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory\ArgsNodeFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\PhpParser\NodeFactory\Service\ServiceOptionNodeFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\VariableName;
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Expression;
+
+/**
+ * Handles this part:
+ *
+ * services:
+ *     SomeNamespace\SomeClass: null <---
+ */
+final class ConfiguredServiceKeyYamlToPhpFactory implements ServiceKeyYamlToPhpFactoryInterface
+{
+    /**
+     * @var ArgsNodeFactory
+     */
+    private $argsNodeFactory;
+
+    /**
+     * @var ServiceOptionNodeFactory
+     */
+    private $serviceOptionNodeFactory;
+
+    public function __construct(ArgsNodeFactory $argsNodeFactory, ServiceOptionNodeFactory $serviceOptionNodeFactory)
+    {
+        $this->argsNodeFactory = $argsNodeFactory;
+        $this->serviceOptionNodeFactory = $serviceOptionNodeFactory;
+    }
+
+    public function convertYamlToNode($key, $yaml): Node
+    {
+        $args = $this->argsNodeFactory->createFromValues([$key]);
+        $methodCall = new MethodCall(new Variable(VariableName::SERVICES), 'set', $args);
+
+        $methodCall = $this->serviceOptionNodeFactory->convertServiceOptionsToNodes($yaml, $methodCall);
+
+        return new Expression($methodCall);
+    }
+
+    public function isMatch($key, $values): bool
+    {
+        if ($key === YamlKey::_DEFAULTS) {
+            return false;
+        }
+
+        if ($key === YamlKey::_INSTANCEOF) {
+            return false;
+        }
+
+        if (isset($values[YamlKey::RESOURCE])) {
+            return false;
+        }
+
+        if ($values === null) {
+            return false;
+        }
+
+        return $values !== [];
+    }
+}
