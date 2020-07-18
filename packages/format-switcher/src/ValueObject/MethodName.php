332c10aa7904b418876883d57886db63a89adf4e
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 23:14:33 +0200

    add MethodName

---
+++ packages/format-switcher/src/ValueObject/MethodName.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher\ValueObject;
+
+final class MethodName
+{
+    /**
+     * @var string
+     */
+    public const SET_METHOD_NAME = 'set';
+}
