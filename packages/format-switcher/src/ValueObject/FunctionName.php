c22e7030ea325191ea39382a098012b8aec724c2
TomasVotruba <tomas.vot@gmail.com>
2020-07-23 14:48:19 +0200

    keep \n as string

--- packages/format-switcher/src/ValueObject/FunctionName.php
+++ packages/format-switcher/src/ValueObject/FunctionName.php
@@ -9,20 +9,20 @@ final class FunctionName
     /**
      * @var string
      */
-    public const INLINE_SERVICE_FUNCTION_NAME = 'Symfony\Component\DependencyInjection\Loader\Configurator\inline_service';
+    public const INLINE_SERVICE = 'Symfony\Component\DependencyInjection\Loader\Configurator\inline_service';
 
     /**
      * @var string
      */
-    public const SERVICE_FUNCTION_NAME = 'Symfony\Component\DependencyInjection\Loader\Configurator\service';
+    public const SERVICE = 'Symfony\Component\DependencyInjection\Loader\Configurator\service';
 
     /**
      * @var string
      */
-    public const REF_FUNCTION_NAME = 'Symfony\Component\DependencyInjection\Loader\Configurator\ref';
+    public const REF = 'Symfony\Component\DependencyInjection\Loader\Configurator\ref';
 
     /**
      * @var string
      */
-    public const EXPR_FUNCTION_NAME = 'Symfony\Component\DependencyInjection\Loader\Configurator\expr';
+    public const EXPR = 'Symfony\Component\DependencyInjection\Loader\Configurator\expr';
 }
