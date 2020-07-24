c0cd329e5f09319288d54698bf0468ffa4acc403
Tomas Votruba <tomas.vot@gmail.com>
2020-07-24 03:11:57 +0200

    Merge pull request #117 from migrify/single-foreach-2

---
+++ packages/format-switcher/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpParser\BuilderFactory;
+use PhpParser\NodeFinder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Yaml\Parser;
+use Symplify\SmartFileSystem\FileSystemFilter;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Migrify\ConfigTransformer\FormatSwitcher\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/DependencyInjection/Loader/*', __DIR__ . '/../src/ValueObject/*']);
+
+    $services->set(BuilderFactory::class);
+
+    $services->set(NodeFinder::class);
+
+    $services->set(Parser::class);
+
+    $services->set(FileSystemFilter::class);
+};
---
+++ packages/format-switcher/src/ConfigLoader.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\FormatSwitcher;
+
+use Migrify\ConfigTransformer\FormatSwitcher\DependencyInjection\Loader\CheckerTolerantYamlFileLoader;
+use Migrify\ConfigTransformer\FormatSwitcher\DependencyInjection\LoaderFactory\IdAwareXmlFileLoaderFactory;
+use Migrify\ConfigTransformer\FormatSwitcher\Exception\NotImplementedYetException;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\ContainerBuilderAndFileContent;
+use Migrify\ConfigTransformer\FormatSwitcher\ValueObject\Format;
+use Nette\Utils\Strings;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+final class ConfigLoader
+{
+    /**
+     * @var IdAwareXmlFileLoaderFactory
+     */
+    private $idAwareXmlFileLoaderFactory;
+
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    public function __construct(
+        IdAwareXmlFileLoaderFactory $idAwareXmlFileLoaderFactory,
+        SmartFileSystem $smartFileSystem
+    ) {
+        $this->idAwareXmlFileLoaderFactory = $idAwareXmlFileLoaderFactory;
+        $this->smartFileSystem = $smartFileSystem;
+    }
+
+    public function createAndLoadContainerBuilderFromFileInfo(
+        SmartFileInfo $smartFileInfo
+    ): ContainerBuilderAndFileContent {
+        $containerBuilder = new ContainerBuilder();
+
+        $loader = $this->createLoaderBySuffix($containerBuilder, $smartFileInfo->getSuffix());
+
+        $fileRealPath = $smartFileInfo->getRealPath();
+
+        // correct old syntax of tags so we can parse it
+        $content = $smartFileInfo->getContents();
+        if (in_array($smartFileInfo->getSuffix(), ['yml', 'yaml'], true)) {
+            $content = Strings::replace($content, '#\!php\/const\:( )?#', '!php/const ');
+            if ($content !== $smartFileInfo->getContents()) {
+                $fileRealPath = sys_get_temp_dir() . '/_migrify_config_tranformer_clean_yaml/' . $smartFileInfo->getFilename();
+                $this->smartFileSystem->dumpFile($fileRealPath, $content);
+            }
+        }
+
+        $loader->load($fileRealPath);
+
+        return new ContainerBuilderAndFileContent($containerBuilder, $content);
+    }
+
+    private function createLoaderBySuffix(ContainerBuilder $containerBuilder, string $suffix): Loader
+    {
+        if ($suffix === Format::XML) {
+            $idAwareXmlFileLoader = $this->idAwareXmlFileLoaderFactory->createFromContainerBuilder($containerBuilder);
+            return $this->wrapToDelegatingLoader($idAwareXmlFileLoader, $containerBuilder);
+        }
+
+        if (in_array($suffix, [Format::YML, Format::YAML], true)) {
+            $yamlFileLoader = new YamlFileLoader($containerBuilder, new FileLocator());
+            return $this->wrapToDelegatingLoader($yamlFileLoader, $containerBuilder);
+        }
+
+        if ($suffix === Format::PHP) {
+            $phpFileLoader = new PhpFileLoader($containerBuilder, new FileLocator());
+            return $this->wrapToDelegatingLoader($phpFileLoader, $containerBuilder);
+        }
+
+        throw new NotImplementedYetException($suffix);
+    }
+
+    private function wrapToDelegatingLoader(Loader $loader, ContainerBuilder $containerBuilder): DelegatingLoader
+    {
+        return new DelegatingLoader(new LoaderResolver([
+            new GlobFileLoader($containerBuilder, new FileLocator()),
+            new PhpFileLoader($containerBuilder, new FileLocator()),
+            // for easier ECS migraiton
+            new CheckerTolerantYamlFileLoader($containerBuilder, new FileLocator()),
+            $loader,
+        ]));
+    }
+}
