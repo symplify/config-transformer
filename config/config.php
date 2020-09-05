63d5e8d89cfe333e994e0c866bd47508e4693f77
Tomas Votruba <tomas.vot@gmail.com>
2020-09-05 18:50:16 +0200

    [MigrifyKernel] Init (#154)

--- config/config.php
+++ config/config.php
@@ -6,13 +6,7 @@ use Migrify\ConfigTransformer\FormatSwitcher\Configuration\Configuration;
 use Migrify\ConfigTransformer\FormatSwitcher\Provider\YamlContentProvider;
 use Migrify\PhpConfigPrinter\Contract\SymfonyVersionFeatureGuardInterface;
 use Migrify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
-use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
-use Symplify\SmartFileSystem\Finder\FinderSanitizer;
-use Symplify\SmartFileSystem\SmartFileSystem;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../packages/**/config/**.php');
@@ -25,18 +19,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autoconfigure();
 
     $services->load('Migrify\ConfigTransformer\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/HttpKernel/*']);
-
-    $services->set(FinderSanitizer::class);
-
-    $services->set(PrivatesAccessor::class);
-
-    $services->set(SmartFileSystem::class);
-
-    $services->set(SymfonyStyleFactory::class);
-
-    $services->set(SymfonyStyle::class)
-        ->factory([ref(SymfonyStyleFactory::class), 'create']);
+        ->exclude([__DIR__ . '/../src/HttpKernel']);
 
     $services->alias(SymfonyVersionFeatureGuardInterface::class, Configuration::class);
 
