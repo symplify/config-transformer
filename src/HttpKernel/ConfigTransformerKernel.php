5ffaf995f7878793101b715c855731f196656d25
Tomas Votruba <tomas.vot@gmail.com>
2020-09-08 23:54:19 +0200

    [CIToGithubActions] Init (#156)

--- src/HttpKernel/ConfigTransformerKernel.php
+++ src/HttpKernel/ConfigTransformerKernel.php
@@ -4,39 +4,13 @@ declare(strict_types=1);
 
 namespace Migrify\ConfigTransformer\HttpKernel;
 
-use Migrify\MigrifyKernel\Bundle\MigrifyKernelBundle;
+use Migrify\MigrifyKernel\HttpKernel\AbstractMigrifyKernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
-use Symfony\Component\HttpKernel\Kernel;
 
-final class ConfigTransformerKernel extends Kernel
+final class ConfigTransformerKernel extends AbstractMigrifyKernel
 {
-    public function __construct(string $environment, bool $debug)
-    {
-        // rand is for container rebuild
-        parent::__construct($environment . random_int(1, 1000), $debug);
-    }
-
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../../config/config.php');
-    }
-
-    public function getCacheDir(): string
-    {
-        return sys_get_temp_dir() . '/_migrify_config_transformer';
-    }
-
-    public function getLogDir(): string
-    {
-        return sys_get_temp_dir() . '/_migrify_config_transformer_log';
-    }
-
-    /**
-     * @return BundleInterface[]
-     */
-    public function registerBundles(): iterable
-    {
-        return [new MigrifyKernelBundle()];
     }
 }
