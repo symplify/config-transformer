78b56cba6f42dd4677afc1b2ceceebbe166d2f56
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 17:41:59 +0200

    [ConfigFeatureBumper] Decouple new package (#172)
    * [ConfigFeatureBumper] Decouple new package
    
    * [ConfigTransformer] Keep the only package
    
    * update sonar paths
    
    * update sonars

---
+++ tests/Converter/ConfigFormatConverter/XmlToPhp/XmlToPhpTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\Tests\Converter\ConfigFormatConverter\XmlToPhp;
+
+use Iterator;
+use Migrify\ConfigTransformer\Tests\Converter\ConfigFormatConverter\AbstractConfigFormatConverterTest;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class XmlToPhpTest extends AbstractConfigFormatConverterTest
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo): void
+    {
+        $this->doTestOutput($fixtureFileInfo, 'xml', 'php');
+    }
+
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.xml');
+    }
+}
