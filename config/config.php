40af6ecd8399336dd3972676e7a8f1be4a4fa30c
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 17:41:59 +0200

    [ConfigFeatureBumper] Decouple new package (#172)
    * [ConfigFeatureBumper] Decouple new package
    
    * [ConfigTransformer] Keep the only package
    
    * update sonar paths
    
    * update sonars

--- config/config.php
+++ config/config.php
@@ -2,14 +2,26 @@
 
 declare(strict_types=1);
 
-use Migrify\ConfigTransformer\FormatSwitcher\Configuration\Configuration;
-use Migrify\ConfigTransformer\FormatSwitcher\Provider\YamlContentProvider;
+use Migrify\ConfigTransformer\Configuration\Configuration;
+use Migrify\ConfigTransformer\Provider\YamlContentProvider;
 use Migrify\PhpConfigPrinter\Contract\SymfonyVersionFeatureGuardInterface;
 use Migrify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
+use PhpParser\BuilderFactory;
+use PhpParser\NodeFinder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Yaml\Parser;
+use Symplify\SmartFileSystem\FileSystemFilter;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/../packages/**/config/**.php');
+    // monorepo
+    $containerConfigurator->import(
+        __DIR__ . '/../../../packages/php-config-printer/config/config.php',
+        null,
+        'not_found'
+    );
+
+    // dependency
+    $containerConfigurator->import(__DIR__ . '/../../php-config-printer/config/config.php', null, 'not_found');
 
     $services = $containerConfigurator->services();
 
@@ -19,9 +31,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autoconfigure();
 
     $services->load('Migrify\ConfigTransformer\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/HttpKernel']);
+        ->exclude([
+            __DIR__ . '/../src/HttpKernel',
+            __DIR__ . '/../src/DependencyInjection/Loader',
+            __DIR__ . '/../src/ValueObject',
+            // configurable class for faking extensions
+            __DIR__ . '/../src/DependencyInjection/Extension/AliasConfigurableExtension.php',
+        ]);
+
+    $services->set(BuilderFactory::class);
+    $services->set(NodeFinder::class);
+    $services->set(Parser::class);
+    $services->set(FileSystemFilter::class);
 
     $services->alias(SymfonyVersionFeatureGuardInterface::class, Configuration::class);
-
     $services->alias(YamlFileContentProviderInterface::class, YamlContentProvider::class);
 };
