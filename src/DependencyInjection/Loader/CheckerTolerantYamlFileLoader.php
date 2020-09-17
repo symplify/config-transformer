77211d0385a6ec76114fdc6802ab2a592cea918b
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 17:41:59 +0200

    [ConfigFeatureBumper] Decouple new package (#172)
    * [ConfigFeatureBumper] Decouple new package
    
    * [ConfigTransformer] Keep the only package
    
    * update sonar paths
    
    * update sonars

---
+++ src/DependencyInjection/Loader/CheckerTolerantYamlFileLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\ConfigTransformer\DependencyInjection\Loader;
+
+use Migrify\PhpConfigPrinter\Yaml\CheckerServiceParametersShifter;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symplify\PackageBuilder\Yaml\FileLoader\AbstractParameterMergingYamlFileLoader;
+
+final class CheckerTolerantYamlFileLoader extends AbstractParameterMergingYamlFileLoader
+{
+    /**
+     * @var CheckerServiceParametersShifter
+     */
+    private $checkerServiceParametersShifter;
+
+    public function __construct(ContainerBuilder $containerBuilder, FileLocatorInterface $fileLocator)
+    {
+        $this->checkerServiceParametersShifter = new CheckerServiceParametersShifter();
+
+        parent::__construct($containerBuilder, $fileLocator);
+    }
+
+    /**
+     * @param string $file
+     * @return mixed|mixed[]
+     */
+    protected function loadFile($file)
+    {
+        /** @var mixed[]|null $configuration */
+        $configuration = parent::loadFile($file);
+        if ($configuration === null) {
+            return [];
+        }
+
+        return $this->checkerServiceParametersShifter->process($configuration);
+    }
+}
