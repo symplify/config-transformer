172ed20c06ba389dee2560c5e8b9ad2475fec775
GitHub <action@github.com>
2021-06-07 08:53:29 +0000

    [ECS] Improve caching (#3284)

--- src/ValueObject/Format.php
+++ src/ValueObject/Format.php
@@ -1,7 +1,8 @@
 <?php
 
-declare (strict_types=1);
-namespace ConfigTransformer20210606\Symplify\ConfigTransformer\ValueObject;
+declare(strict_types=1);
+
+namespace Symplify\ConfigTransformer\ValueObject;
 
 final class Format
 {
@@ -9,14 +10,17 @@ final class Format
      * @var string
      */
     public const YAML = 'yaml';
+
     /**
      * @var string
      */
     public const YML = 'yml';
+
     /**
      * @var string
      */
     public const XML = 'xml';
+
     /**
      * @var string
      */
