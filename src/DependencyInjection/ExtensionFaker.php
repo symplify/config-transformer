cc7755373f4b84f0081ee5265c8aa7da157a7a4a
GitHub Action <action@github.com>
2021-06-08 17:40:03 +0000

    Updated Config Transformer to commit 5456a4ba6e81db2e2258f4cf103d4e1bb75cf083
    https://github.com/symplify/symplify/commit/5456a4ba6e81db2e2258f4cf103d4e1bb75cf083 [PHPStanRules] Add ForbiddenClassConstRule (#3294)

--- src/DependencyInjection/ExtensionFaker.php
+++ src/DependencyInjection/ExtensionFaker.php
@@ -1,14 +1,12 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace ConfigTransformer20210608\Symplify\ConfigTransformer\DependencyInjection;
 
-namespace Symplify\ConfigTransformer\DependencyInjection;
-
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Yaml\Yaml;
-use Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension;
-use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
-
+use ConfigTransformer20210608\Symfony\Component\DependencyInjection\ContainerBuilder;
+use ConfigTransformer20210608\Symfony\Component\Yaml\Yaml;
+use ConfigTransformer20210608\Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension;
+use ConfigTransformer20210608\Symplify\PhpConfigPrinter\ValueObject\YamlKey;
 /**
  * This fakes basic extensions, so loading of config is possible without loading real extensions and booting your whole
  * project
@@ -19,30 +17,25 @@ final class ExtensionFaker
      * @var YamlKey
      */
     private $yamlKey;
-
-    public function __construct(YamlKey $yamlKey)
+    public function __construct(\ConfigTransformer20210608\Symplify\PhpConfigPrinter\ValueObject\YamlKey $yamlKey)
     {
         $this->yamlKey = $yamlKey;
     }
-
-    public function fakeInContainerBuilder(ContainerBuilder $containerBuilder, string $yamlContent): void
+    public function fakeInContainerBuilder(\ConfigTransformer20210608\Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder, string $yamlContent) : void
     {
-        $yaml = Yaml::parse($yamlContent, Yaml::PARSE_CUSTOM_TAGS);
+        $yaml = \ConfigTransformer20210608\Symfony\Component\Yaml\Yaml::parse($yamlContent, \ConfigTransformer20210608\Symfony\Component\Yaml\Yaml::PARSE_CUSTOM_TAGS);
         // empty file
         if ($yaml === null) {
             return;
         }
-
-        $rootKeys = array_keys($yaml);
-
+        $rootKeys = \array_keys($yaml);
         /** @var string[] $extensionKeys */
-        $extensionKeys = array_diff($rootKeys, $this->yamlKey->provideRootKeys());
+        $extensionKeys = \array_diff($rootKeys, $this->yamlKey->provideRootKeys());
         if ($extensionKeys === []) {
             return;
         }
-
         foreach ($extensionKeys as $extensionKey) {
-            $aliasConfigurableExtension = new AliasConfigurableExtension($extensionKey);
+            $aliasConfigurableExtension = new \ConfigTransformer20210608\Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension($extensionKey);
             $containerBuilder->registerExtension($aliasConfigurableExtension);
         }
     }
--- src/Provider/YamlContentProvider.php
+++ src/Provider/YamlContentProvider.php
@@ -1,30 +1,25 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace ConfigTransformer20210608\Symplify\ConfigTransformer\Provider;
 
-namespace Symplify\ConfigTransformer\Provider;
-
-use Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
-use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
-
-final class YamlContentProvider implements YamlFileContentProviderInterface
+use ConfigTransformer20210608\Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
+use ConfigTransformer20210608\Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
+final class YamlContentProvider implements \ConfigTransformer20210608\Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface
 {
     /**
      * @var string|null
      */
     private $yamlContent;
-
-    public function setContent(string $yamlContent): void
+    public function setContent(string $yamlContent) : void
     {
         $this->yamlContent = $yamlContent;
     }
-
-    public function getYamlContent(): string
+    public function getYamlContent() : string
     {
         if ($this->yamlContent === null) {
-            throw new ShouldNotHappenException();
+            throw new \ConfigTransformer20210608\Symplify\SymplifyKernel\Exception\ShouldNotHappenException();
         }
-
         return $this->yamlContent;
     }
 }
