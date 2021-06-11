7a7c9318e97dec58c0d75dea3844f616fb5e07ae
GitHub <action@github.com>
2021-06-11 17:27:13 +0000

    [PHPStanRules] Fix Psr4PathValidator (#3304)

--- vendor/nikic/php-parser/lib/PhpParser/Node/Scalar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106114\PhpParser\Node;
-
-abstract class Scalar extends \ConfigTransformer202106114\PhpParser\Node\Expr
-{
-}
--- vendor/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106114\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
