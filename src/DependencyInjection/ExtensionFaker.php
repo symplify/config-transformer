f01cb90e888d06a0574a45d77c2599b8abbb075b
GitHub <action@github.com>
2021-06-11 14:09:50 +0000

    [ECS] [ConfigTransformer] Bump code syntax to PHP 8 (#3303)

--- src/DependencyInjection/ExtensionFaker.php
+++ src/DependencyInjection/ExtensionFaker.php
@@ -1,41 +1,43 @@
 <?php
 
-declare (strict_types=1);
-namespace ConfigTransformer2021061110\Symplify\ConfigTransformer\DependencyInjection;
+declare(strict_types=1);
 
-use ConfigTransformer2021061110\Symfony\Component\DependencyInjection\ContainerBuilder;
-use ConfigTransformer2021061110\Symfony\Component\Yaml\Yaml;
-use ConfigTransformer2021061110\Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension;
-use ConfigTransformer2021061110\Symplify\PhpConfigPrinter\ValueObject\YamlKey;
+namespace Symplify\ConfigTransformer\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Yaml;
+use Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension;
+use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
+
 /**
  * This fakes basic extensions, so loading of config is possible without loading real extensions and booting your whole
  * project
  */
 final class ExtensionFaker
 {
-    /**
-     * @var YamlKey
-     */
-    private $yamlKey;
-    public function __construct(\ConfigTransformer2021061110\Symplify\PhpConfigPrinter\ValueObject\YamlKey $yamlKey)
-    {
-        $this->yamlKey = $yamlKey;
+    public function __construct(
+        private YamlKey $yamlKey
+    ) {
     }
-    public function fakeInContainerBuilder(\ConfigTransformer2021061110\Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder, string $yamlContent) : void
+
+    public function fakeInContainerBuilder(ContainerBuilder $containerBuilder, string $yamlContent): void
     {
-        $yaml = \ConfigTransformer2021061110\Symfony\Component\Yaml\Yaml::parse($yamlContent, \ConfigTransformer2021061110\Symfony\Component\Yaml\Yaml::PARSE_CUSTOM_TAGS);
+        $yaml = Yaml::parse($yamlContent, Yaml::PARSE_CUSTOM_TAGS);
         // empty file
         if ($yaml === null) {
             return;
         }
-        $rootKeys = \array_keys($yaml);
+
+        $rootKeys = array_keys($yaml);
+
         /** @var string[] $extensionKeys */
-        $extensionKeys = \array_diff($rootKeys, $this->yamlKey->provideRootKeys());
+        $extensionKeys = array_diff($rootKeys, $this->yamlKey->provideRootKeys());
         if ($extensionKeys === []) {
             return;
         }
+
         foreach ($extensionKeys as $extensionKey) {
-            $aliasConfigurableExtension = new \ConfigTransformer2021061110\Symplify\ConfigTransformer\DependencyInjection\Extension\AliasConfigurableExtension($extensionKey);
+            $aliasConfigurableExtension = new AliasConfigurableExtension($extensionKey);
             $containerBuilder->registerExtension($aliasConfigurableExtension);
         }
     }
--- vendor/nikic/php-parser/lib/PhpParser/Node/Scalar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer2021061110\PhpParser\Node;
-
-abstract class Scalar extends \ConfigTransformer2021061110\PhpParser\Node\Expr
-{
-}
