b5e58286f16142dcda990970be9f94b84c388a2d
GitHub Action <action@github.com>
2021-06-11 22:58:27 +0000

    Updated Config Transformer to commit 3379dd66c5a5dc049875bcf2a4ad0e31293a49e6
    https://github.com/symplify/symplify/commit/3379dd66c5a5dc049875bcf2a4ad0e31293a49e6 move FullyQualifyingPhpDocNodeVisitor to SimplePhpDocParser

---
+++ vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
@@ -0,0 +1,9 @@
+<?php
+
+declare (strict_types=1);
+namespace ConfigTransformer202106111\PhpParser\Node;
+
+use ConfigTransformer202106111\PhpParser\NodeAbstract;
+abstract class Stmt extends \ConfigTransformer202106111\PhpParser\NodeAbstract
+{
+}
---
+++ vendor/symplify/easy-testing/src/Finder/FixtureFinder.php
@@ -0,0 +1,28 @@
+<?php
+
+declare (strict_types=1);
+namespace ConfigTransformer202106111\Symplify\EasyTesting\Finder;
+
+use ConfigTransformer202106111\Symfony\Component\Finder\Finder;
+use ConfigTransformer202106111\Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use ConfigTransformer202106111\Symplify\SmartFileSystem\SmartFileInfo;
+final class FixtureFinder
+{
+    /**
+     * @var FinderSanitizer
+     */
+    private $finderSanitizer;
+    public function __construct(\ConfigTransformer202106111\Symplify\SmartFileSystem\Finder\FinderSanitizer $finderSanitizer)
+    {
+        $this->finderSanitizer = $finderSanitizer;
+    }
+    /**
+     * @return SmartFileInfo[]
+     */
+    public function find(array $sources) : array
+    {
+        $finder = new \ConfigTransformer202106111\Symfony\Component\Finder\Finder();
+        $finder->files()->in($sources)->name('*.php.inc')->path('Fixture')->sortByName();
+        return $this->finderSanitizer->sanitize($finder);
+    }
+}
