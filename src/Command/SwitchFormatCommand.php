564016747360452d2c53d69d48603ad5d7b288dc
GitHub <action@github.com>
2021-06-11 23:01:55 +0000

    Revert "move FullyQualifyingPhpDocNodeVisitor to SimplePhpDocParser"

--- src/Command/SwitchFormatCommand.php
+++ src/Command/SwitchFormatCommand.php
@@ -1,61 +1,72 @@
 <?php
 
-declare (strict_types=1);
-namespace ConfigTransformer202106111\Symplify\ConfigTransformer\Command;
+declare(strict_types=1);
 
-use ConfigTransformer202106111\Symfony\Component\Console\Input\InputArgument;
-use ConfigTransformer202106111\Symfony\Component\Console\Input\InputInterface;
-use ConfigTransformer202106111\Symfony\Component\Console\Input\InputOption;
-use ConfigTransformer202106111\Symfony\Component\Console\Output\OutputInterface;
-use ConfigTransformer202106111\Symplify\ConfigTransformer\Configuration\Configuration;
-use ConfigTransformer202106111\Symplify\ConfigTransformer\Converter\ConvertedContentFactory;
-use ConfigTransformer202106111\Symplify\ConfigTransformer\FileSystem\ConfigFileDumper;
-use ConfigTransformer202106111\Symplify\ConfigTransformer\ValueObject\Option;
-use ConfigTransformer202106111\Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
-use ConfigTransformer202106111\Symplify\PackageBuilder\Console\ShellCode;
-final class SwitchFormatCommand extends \ConfigTransformer202106111\Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand
+namespace Symplify\ConfigTransformer\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\ConfigTransformer\Configuration\Configuration;
+use Symplify\ConfigTransformer\Converter\ConvertedContentFactory;
+use Symplify\ConfigTransformer\FileSystem\ConfigFileDumper;
+use Symplify\ConfigTransformer\ValueObject\Option;
+use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
+use Symplify\PackageBuilder\Console\ShellCode;
+
+final class SwitchFormatCommand extends AbstractSymplifyCommand
 {
-    /**
-     * @var \Symplify\ConfigTransformer\Configuration\Configuration
-     */
-    private $configuration;
-    /**
-     * @var \Symplify\ConfigTransformer\FileSystem\ConfigFileDumper
-     */
-    private $configFileDumper;
-    /**
-     * @var \Symplify\ConfigTransformer\Converter\ConvertedContentFactory
-     */
-    private $convertedContentFactory;
-    public function __construct(\ConfigTransformer202106111\Symplify\ConfigTransformer\Configuration\Configuration $configuration, \ConfigTransformer202106111\Symplify\ConfigTransformer\FileSystem\ConfigFileDumper $configFileDumper, \ConfigTransformer202106111\Symplify\ConfigTransformer\Converter\ConvertedContentFactory $convertedContentFactory)
-    {
-        $this->configuration = $configuration;
-        $this->configFileDumper = $configFileDumper;
-        $this->convertedContentFactory = $convertedContentFactory;
+    public function __construct(
+        private Configuration $configuration,
+        private ConfigFileDumper $configFileDumper,
+        private ConvertedContentFactory $convertedContentFactory
+    ) {
         parent::__construct();
     }
-    protected function configure() : void
+
+    protected function configure(): void
     {
         $this->setDescription('Converts XML/YAML configs to PHP format');
-        $this->addArgument(\ConfigTransformer202106111\Symplify\ConfigTransformer\ValueObject\Option::SOURCES, \ConfigTransformer202106111\Symfony\Component\Console\Input\InputArgument::REQUIRED | \ConfigTransformer202106111\Symfony\Component\Console\Input\InputArgument::IS_ARRAY, 'Path to directory with configs');
-        $this->addOption(\ConfigTransformer202106111\Symplify\ConfigTransformer\ValueObject\Option::TARGET_SYMFONY_VERSION, 's', \ConfigTransformer202106111\Symfony\Component\Console\Input\InputOption::VALUE_REQUIRED, 'Symfony version to migrate config to', '3.2');
-        $this->addOption(\ConfigTransformer202106111\Symplify\ConfigTransformer\ValueObject\Option::DRY_RUN, null, \ConfigTransformer202106111\Symfony\Component\Console\Input\InputOption::VALUE_NONE, 'Dry run - no removal or config change');
+
+        $this->addArgument(
+            Option::SOURCES,
+            InputArgument::REQUIRED | InputArgument::IS_ARRAY,
+            'Path to directory with configs'
+        );
+
+        $this->addOption(
+            Option::TARGET_SYMFONY_VERSION,
+            's',
+            InputOption::VALUE_REQUIRED,
+            'Symfony version to migrate config to',
+            '3.2'
+        );
+
+        $this->addOption(Option::DRY_RUN, null, InputOption::VALUE_NONE, 'Dry run - no removal or config change');
     }
-    protected function execute(\ConfigTransformer202106111\Symfony\Component\Console\Input\InputInterface $input, \ConfigTransformer202106111\Symfony\Component\Console\Output\OutputInterface $output) : int
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->configuration->populateFromInput($input);
+
         $suffixes = $this->configuration->getInputSuffixes();
-        $suffixesRegex = '#\\.' . \implode('|', $suffixes) . '$#';
+        $suffixesRegex = '#\.' . implode('|', $suffixes) . '$#';
         $fileInfos = $this->smartFinder->find($this->configuration->getSource(), $suffixesRegex);
+
         $convertedContents = $this->convertedContentFactory->createFromFileInfos($fileInfos);
+
         foreach ($convertedContents as $convertedContent) {
             $this->configFileDumper->dumpFile($convertedContent);
         }
-        if (!$this->configuration->isDryRun()) {
+
+        if (! $this->configuration->isDryRun()) {
             $this->smartFileSystem->remove($fileInfos);
         }
-        $successMessage = \sprintf('Processed %d file(s) to "PHP" format', \count($fileInfos));
+
+        $successMessage = sprintf('Processed %d file(s) to "PHP" format', count($fileInfos));
         $this->symfonyStyle->success($successMessage);
-        return \ConfigTransformer202106111\Symplify\PackageBuilder\Console\ShellCode::SUCCESS;
+
+        return ShellCode::SUCCESS;
     }
 }
