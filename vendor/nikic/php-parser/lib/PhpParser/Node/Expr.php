6fbd4e17df9dfe8e6337269af1c2fbf301301609
GitHub <action@github.com>
2021-06-12 08:34:32 +0000

    cleanup

--- vendor/nikic/php-parser/lib/PhpParser/Node/Expr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106122\PhpParser\Node;
-
-use ConfigTransformer202106122\PhpParser\NodeAbstract;
-abstract class Expr extends \ConfigTransformer202106122\PhpParser\NodeAbstract
-{
-}
--- vendor/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106122\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
