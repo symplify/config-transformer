5a05bb86677d0f25cd11a99926248bee82be0e92
GitHub <action@github.com>
2021-06-12 08:27:16 +0000

    last touches

--- vendor/nikic/php-parser/lib/PhpParser/Node/Expr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106123\PhpParser\Node;
-
-use ConfigTransformer202106123\PhpParser\NodeAbstract;
-abstract class Expr extends \ConfigTransformer202106123\PhpParser\NodeAbstract
-{
-}
