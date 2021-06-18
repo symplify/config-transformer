8beb634d1e8cb30efbd87c5874d9a977b7d54569
GitHub <action@github.com>
2021-06-18 16:06:45 +0000

    [ECS] Prepare for parallel by passing error/diff result instantly instead of using service juggling (#3325)

--- vendor/nikic/php-parser/lib/PhpParser/ConstExprEvaluationException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106184\PhpParser;
-
-class ConstExprEvaluationException extends \Exception
-{
-}
--- vendor/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106184\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
