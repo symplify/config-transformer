e049279c64f41b1ed61c00a63b479e5a06027fbc
GitHub <action@github.com>
2021-06-19 08:26:43 +0000

    logo

--- src/Provider/YamlContentProvider.php
+++ src/Provider/YamlContentProvider.php
@@ -1,25 +1,27 @@
 <?php
 
-declare (strict_types=1);
-namespace ConfigTransformer202106187\Symplify\ConfigTransformer\Provider;
+declare(strict_types=1);
 
-use ConfigTransformer202106187\Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
-use ConfigTransformer202106187\Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
-final class YamlContentProvider implements \ConfigTransformer202106187\Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface
+namespace Symplify\ConfigTransformer\Provider;
+
+use Symplify\PhpConfigPrinter\Contract\YamlFileContentProviderInterface;
+use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
+
+final class YamlContentProvider implements YamlFileContentProviderInterface
 {
-    /**
-     * @var string|null
-     */
-    private $yamlContent;
-    public function setContent(string $yamlContent) : void
+    private ?string $yamlContent = null;
+
+    public function setContent(string $yamlContent): void
     {
         $this->yamlContent = $yamlContent;
     }
-    public function getYamlContent() : string
+
+    public function getYamlContent(): string
     {
         if ($this->yamlContent === null) {
-            throw new \ConfigTransformer202106187\Symplify\SymplifyKernel\Exception\ShouldNotHappenException();
+            throw new ShouldNotHappenException();
         }
+
         return $this->yamlContent;
     }
 }
--- vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106187\PhpParser\Node;
-
-use ConfigTransformer202106187\PhpParser\NodeAbstract;
-abstract class Stmt extends \ConfigTransformer202106187\PhpParser\NodeAbstract
-{
-}
