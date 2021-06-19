67b4958f5bfd9afcdb7e57a47947d59d7c4947d2
GitHub <action@github.com>
2021-06-19 16:33:12 +0000

    [ECS] Towards parallel #3 - turn Configuration service into the immutable Value Object (#3332)

--- vendor/nikic/php-parser/lib/PhpParser/ConstExprEvaluationException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106194\PhpParser;
-
-class ConstExprEvaluationException extends \Exception
-{
-}
--- vendor/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106194\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
