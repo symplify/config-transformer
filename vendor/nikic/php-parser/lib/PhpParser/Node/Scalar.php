d656fe194f75791007362cbbe164952a3c106c0e
GitHub <action@github.com>
2021-06-20 13:50:10 +0000

    [ECS] Making Encode/Decode aproach work with json - part #4 (#3334)

--- vendor/nikic/php-parser/lib/PhpParser/Node/Scalar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106194\PhpParser\Node;
-
-abstract class Scalar extends \ConfigTransformer202106194\PhpParser\Node\Expr
-{
-}
--- vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106194\PhpParser\Node;
-
-use ConfigTransformer202106194\PhpParser\NodeAbstract;
-abstract class Stmt extends \ConfigTransformer202106194\PhpParser\NodeAbstract
-{
-}
