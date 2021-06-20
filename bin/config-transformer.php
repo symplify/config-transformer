e5c78a32bfda3874ed2a1a90184d1a4ed3149b17
GitHub <action@github.com>
2021-06-20 19:53:55 +0000

    [ECS] Make connection in process and subprocess - step #5 (#3335)

--- bin/config-transformer.php
+++ bin/config-transformer.php
@@ -1,10 +1,10 @@
 <?php
 
-declare (strict_types=1);
-namespace ConfigTransformer202106205;
+declare(strict_types=1);
 
-use ConfigTransformer202106205\Symplify\ConfigTransformer\HttpKernel\ConfigTransformerKernel;
-use ConfigTransformer202106205\Symplify\SymplifyKernel\ValueObject\KernelBootAndApplicationRun;
+use Symplify\ConfigTransformer\HttpKernel\ConfigTransformerKernel;
+use Symplify\SymplifyKernel\ValueObject\KernelBootAndApplicationRun;
+
 $possibleAutoloadPaths = [
     // after split package
     __DIR__ . '/../vendor/autoload.php',
@@ -13,19 +13,25 @@ $possibleAutoloadPaths = [
     // monorepo
     __DIR__ . '/../../../vendor/autoload.php',
 ];
+
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
-    if (\file_exists($possibleAutoloadPath)) {
+    if (file_exists($possibleAutoloadPath)) {
         require_once $possibleAutoloadPath;
         break;
     }
 }
+
+
 $scoperAutoloadFilepath = __DIR__ . '/../vendor/scoper-autoload.php';
-if (\file_exists($scoperAutoloadFilepath)) {
+if (file_exists($scoperAutoloadFilepath)) {
     require_once $scoperAutoloadFilepath;
 }
-$codeSnifferAutoload = \getcwd() . '/vendor/squizlabs/php_codesniffer/autoload.php';
-if (\file_exists($codeSnifferAutoload)) {
+
+
+$codeSnifferAutoload = getcwd() . '/vendor/squizlabs/php_codesniffer/autoload.php';
+if (file_exists($codeSnifferAutoload)) {
     require_once $codeSnifferAutoload;
 }
-$kernelBootAndApplicationRun = new \ConfigTransformer202106205\Symplify\SymplifyKernel\ValueObject\KernelBootAndApplicationRun(\ConfigTransformer202106205\Symplify\ConfigTransformer\HttpKernel\ConfigTransformerKernel::class);
+
+$kernelBootAndApplicationRun = new KernelBootAndApplicationRun(ConfigTransformerKernel::class);
 $kernelBootAndApplicationRun->run();
--- vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106205\PhpParser\Node;
-
-use ConfigTransformer202106205\PhpParser\NodeAbstract;
-abstract class Stmt extends \ConfigTransformer202106205\PhpParser\NodeAbstract
-{
-}
