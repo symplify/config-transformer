fee20a8f8990446e371177b3ffe740104da0548a
GitHub <action@github.com>
2021-06-27 23:29:57 +0000

    fix version in changed fiels

--- vendor/nikic/php-parser/lib/PhpParser/ConstExprEvaluationException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106266\PhpParser;
-
-class ConstExprEvaluationException extends \Exception
-{
-}
--- vendor/symplify/package-builder/src/Exception/HttpKernel/MissingInterfaceException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106266\Symplify\PackageBuilder\Exception\HttpKernel;
-
-use Exception;
-final class MissingInterfaceException extends \Exception
-{
-}
