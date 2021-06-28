6d8e65f66945388e0d99e9efbe3b76b46e3de3ce
GitHub <action@github.com>
2021-06-28 11:03:50 +0000

    [PhpConfigPrinter] Properly import function calls (#3344)

--- vendor/nikic/php-parser/lib/PhpParser/Comment/Doc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106287\PhpParser\Comment;
-
-class Doc extends \ConfigTransformer202106287\PhpParser\Comment
-{
-}
--- vendor/nikic/php-parser/lib/PhpParser/ConstExprEvaluationException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ConfigTransformer202106287\PhpParser;
-
-class ConstExprEvaluationException extends \Exception
-{
-}
