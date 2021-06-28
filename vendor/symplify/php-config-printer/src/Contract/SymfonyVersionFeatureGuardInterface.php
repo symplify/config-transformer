8b1367092df0635e3c8fd3207f8156f5c46a3aa6
GitHub <action@github.com>
2021-06-28 14:40:45 +0000

    [ESC] Rebuild cache just once per release (#3359)

--- vendor/symplify/php-config-printer/src/Contract/SymfonyVersionFeatureGuardInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106289\Symplify\PhpConfigPrinter\Contract;
-
-interface SymfonyVersionFeatureGuardInterface
-{
-    public function isAtLeastSymfonyVersion(float $symfonyVersion) : bool;
-}
