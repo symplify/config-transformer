dc845ed4cb3374c3e5dc7cdd09986f43633f25cf
GitHub <action@github.com>
2021-06-28 12:58:22 +0000

    [ESC] Fix missing require downgrade - ref https://github.com/symplify/symplify/issues/3355#issuecomment-869640777

--- vendor/nikic/php-parser/lib/PhpParser/Node/Expr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106287\PhpParser\Node;
-
-use ConfigTransformer202106287\PhpParser\NodeAbstract;
-abstract class Expr extends \ConfigTransformer202106287\PhpParser\NodeAbstract
-{
-}
