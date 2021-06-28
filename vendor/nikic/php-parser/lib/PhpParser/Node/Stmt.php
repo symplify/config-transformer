0e0fe9e658c0db9929f57992684762366cb6c68f
GitHub <action@github.com>
2021-06-28 20:27:46 +0000

    [PHPStanRules] allow enums to have @method annotation (#3361)

--- vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106282\PhpParser\Node;
-
-use ConfigTransformer202106282\PhpParser\NodeAbstract;
-abstract class Stmt extends \ConfigTransformer202106282\PhpParser\NodeAbstract
-{
-}
--- vendor/symplify/symplify-kernel/src/Exception/HttpKernel/TooGenericKernelClassException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106282\Symplify\SymplifyKernel\Exception\HttpKernel;
-
-use Exception;
-final class TooGenericKernelClassException extends \Exception
-{
-}
