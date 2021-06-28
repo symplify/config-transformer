7a012aecbd5be9d4bb8721676190215e5db1fd81
GitHub <action@github.com>
2021-06-28 14:21:34 +0000

    [Coding Standard] Reimplement checking for arrow function support (#3358)

--- vendor/nikic/php-parser/lib/PhpParser/Node/Stmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace ConfigTransformer202106289\PhpParser\Node;
-
-use ConfigTransformer202106289\PhpParser\NodeAbstract;
-abstract class Stmt extends \ConfigTransformer202106289\PhpParser\NodeAbstract
-{
-}
