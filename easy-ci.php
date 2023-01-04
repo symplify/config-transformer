40a9cee0960cba837e9dad3ef416232b0af0ce3a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-04 20:47:59 +0700

    easy-ci type to skip

--- easy-ci.php
+++ easy-ci.php
@@ -2,13 +2,10 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Console\Application;
 use Symplify\EasyCI\Config\EasyCIConfig;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        AbstractSymplifyKernel::class,
-        Application::class,
+        \Symplify\ConfigTransformer\NodeVisitor\RefOrServiceFuncCallPrePrintNodeVisitor::class,
     ]);
 };
