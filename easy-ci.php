1704d432ccb2c5f964c7b5224d1e9717a785db51
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-04 20:54:21 +0700

    fix type to skip

--- easy-ci.php
+++ easy-ci.php
@@ -7,5 +7,7 @@ use Symplify\EasyCI\Config\EasyCIConfig;
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
         \Symplify\ConfigTransformer\NodeVisitor\RefOrServiceFuncCallPrePrintNodeVisitor::class,
+        \Symplify\ConfigTransformer\Console\ConfigTransformerApplication::class,
+        \Symplify\ConfigTransformer\Kernel\ConfigTransformerKernel::class,
     ]);
 };
