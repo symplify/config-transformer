6e22a221195691a3868f7ddb39d4ffe15f5ddbc7
GitHub Action <action@github.com>
2023-12-16 17:48:03 +0000

    release PHP 7.2 downgraded

--- src/ValueObject/Option.php
+++ src/ValueObject/Option.php
@@ -1,7 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Symplify\ConfigTransformer\ValueObject;
 
 final class Option
@@ -10,7 +9,6 @@ final class Option
      * @var string
      */
     public const DRY_RUN = 'dry-run';
-
     /**
      * @var string
      */
