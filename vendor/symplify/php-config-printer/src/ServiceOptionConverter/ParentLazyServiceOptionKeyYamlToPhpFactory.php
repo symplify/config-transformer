776589cf396d4eda30d91c839e6a351c3da0762f
GitHub Action <action@github.com>
2023-12-16 17:13:17 +0000

    release PHP 7.2 downgraded

---
+++ vendor/symplify/php-config-printer/src/ServiceOptionConverter/ParentLazyServiceOptionKeyYamlToPhpFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare (strict_types=1);
+namespace Symplify\PhpConfigPrinter\ServiceOptionConverter;
+
+use ConfigTransformerPrefix202312\PhpParser\BuilderHelpers;
+use ConfigTransformerPrefix202312\PhpParser\Node\Arg;
+use ConfigTransformerPrefix202312\PhpParser\Node\Expr\MethodCall;
+use Symplify\PhpConfigPrinter\Contract\Converter\ServiceOptionsKeyYamlToPhpFactoryInterface;
+use Symplify\PhpConfigPrinter\ValueObject\YamlKey;
+final class ParentLazyServiceOptionKeyYamlToPhpFactory implements ServiceOptionsKeyYamlToPhpFactoryInterface
+{
+    /**
+     * @param mixed $key
+     * @param mixed $yaml
+     * @param mixed $values
+     */
+    public function decorateServiceMethodCall($key, $yaml, $values, MethodCall $methodCall) : MethodCall
+    {
+        $args = [new Arg(BuilderHelpers::normalizeValue($values[$key]))];
+        return new MethodCall($methodCall, $key, $args);
+    }
+    /**
+     * @param mixed $key
+     * @param mixed $values
+     */
+    public function isMatch($key, $values) : bool
+    {
+        return \in_array($key, [YamlKey::PARENT, YamlKey::LAZY], \true);
+    }
+}
---
+++ vendor/symplify/php-config-printer/src/ServiceOptionConverter/PropertiesServiceOptionKeyYamlToPhpFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare (strict_types=1);
+namespace Symplify\PhpConfigPrinter\ServiceOptionConverter;
+
+use ConfigTransformerPrefix202312\PhpParser\Node\Expr\MethodCall;
+use Symplify\PhpConfigPrinter\Contract\Converter\ServiceOptionsKeyYamlToPhpFactoryInterface;
+use Symplify\PhpConfigPrinter\NodeFactory\Service\SingleServicePhpNodeFactory;
+use Symplify\PhpConfigPrinter\ValueObject\YamlServiceKey;
+final class PropertiesServiceOptionKeyYamlToPhpFactory implements ServiceOptionsKeyYamlToPhpFactoryInterface
+{
+    /**
+     * @readonly
+     * @var \Symplify\PhpConfigPrinter\NodeFactory\Service\SingleServicePhpNodeFactory
+     */
+    private $singleServicePhpNodeFactory;
+    public function __construct(SingleServicePhpNodeFactory $singleServicePhpNodeFactory)
+    {
+        $this->singleServicePhpNodeFactory = $singleServicePhpNodeFactory;
+    }
+    /**
+     * @param mixed $key
+     * @param mixed $yaml
+     * @param mixed $values
+     */
+    public function decorateServiceMethodCall($key, $yaml, $values, MethodCall $methodCall) : MethodCall
+    {
+        return $this->singleServicePhpNodeFactory->createProperties($methodCall, $yaml);
+    }
+    /**
+     * @param mixed $key
+     * @param mixed $values
+     */
+    public function isMatch($key, $values) : bool
+    {
+        return $key === YamlServiceKey::PROPERTIES;
+    }
+}
