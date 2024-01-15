4f790f1432a39b6a8333aab98b9fb57ce6877402
Tomas Votruba <tomas.vot@gmail.com>
2024-01-15 15:50:04 +0100

    tidy build

--- build/rector-downgrade-php-72.php
+++ build/rector-downgrade-php-72.php
@@ -6,16 +6,14 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->parallel(240, 8, 1);
-
     $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
     $rectorConfig->skip([
-                            '*/Tests/*',
-                            '*/tests/*',
-                            __DIR__ . '/../../tests',
-                            # missing "optional" dependency and never used here
-                            '*/symfony/framework-bundle/KernelBrowser.php',
-                            '*/symfony/http-kernel/HttpKernelBrowser.php',
+            '*/Tests/*',
+            '*/tests/*',
+            __DIR__ . '/../../tests',
+            # missing "optional" dependency and never used here
+            '*/symfony/framework-bundle/KernelBrowser.php',
+            '*/symfony/http-kernel/HttpKernelBrowser.php',
     ]);
 };
