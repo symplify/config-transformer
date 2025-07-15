b1e409f97ee52bdef5c5233a6cfc236540c5b7ce
Tomas Votruba <tomas.vot@gmail.com>
2025-07-15 09:40:44 +0200

    nicer output

--- src/FileSystem/ConfigFileDumper.php
+++ src/FileSystem/ConfigFileDumper.php
@@ -18,16 +18,13 @@ final class ConfigFileDumper
 
     public function dumpFile(ConvertedContent $convertedContent, Configuration $configuration): void
     {
-        if ($configuration->isDryRun()) {
-            // report only
-            $this->symfonyStyle->writeln($convertedContent->getOriginalRelativeFilePath() . ' would be removed');
-            $this->symfonyStyle->writeln($convertedContent->getNewRelativeFilePath() . ' would be added');
-            $this->symfonyStyle->newLine();
-            return;
-        }
-
-        $this->symfonyStyle->writeln($convertedContent->getOriginalRelativeFilePath() . ' removed');
-        $this->symfonyStyle->writeln($convertedContent->getNewRelativeFilePath() . ' added');
+        $this->symfonyStyle->writeln(sprintf(
+            'Converting "<fg=yellow>%s</>"%sto"<fg=green>%s</>"%s',
+            $convertedContent->getOriginalRelativeFilePath(),
+            PHP_EOL,
+            $convertedContent->getNewRelativeFilePath(),
+            $configuration->isDryRun() ? ' (dry run)' : ''
+        ));
         $this->symfonyStyle->newLine();
 
         $originalFilePathWithoutSuffix = $convertedContent->getOriginalFilePathWithoutSuffix();
